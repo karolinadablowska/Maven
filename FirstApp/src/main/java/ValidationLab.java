62def0199a3723e2dea05009e817ff5fd2b89809
karolinadablowska <karolina.dablowska@hotmail.com>
2020-10-27 14:58:17 +0100

    second commit

---
+++ FirstApp/src/main/java/ValidationLab.java
@@ -0,0 +1,35 @@
+import java.util.regex.Pattern;
+
+public class ValidationLab {
+
+
+
+    public static void main (String [] args) {
+
+    boolean answer = validateEmail("karolina.dablowskadd");
+    boolean answer2 = validatePhoneNumber("730830893");
+
+        System.out.println(answer);
+        System.out.println(answer2);
+
+    }
+
+
+
+    static boolean validateEmail(String mail) {
+        Pattern pattern = Pattern.compile(".+@.+\\.com.");
+        if (pattern.matcher(mail).matches()) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile("^\\d{9}$");
+        if (pattern.matcher(phoneNumber).matches()) {
+            return true;
+        }
+        return false;
+
+            }
+}
