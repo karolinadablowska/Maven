56a4bd6192a3fb382b5478d3b0f1463e47439dea
karolinadablowska <68810645+karolinadablowska@users.noreply.github.com>
2020-11-01 15:39:14 +0100

    Estate company with Tests

---
+++ EstateCompany/src/main/java/OfficeOfProperty.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class OfficeOfProperty {
+
+
+    private int iterator = 0;
+    private Property[] properties;
+
+    public OfficeOfProperty(int propertySize) {
+        this.properties = new Property[propertySize];
+    }
+
+    public OfficeOfProperty() {
+
+    }
+
+    public void addProperty(Property property) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        String name = sc.next();
+
+        System.out.println("Enter area: ");
+        double area = sc.nextDouble();
+
+        System.out.println("Enter price:");
+        double price = sc.nextDouble();
+
+
+         }
+
+    public int howManyProperties() {
+        return iterator;
+    }
+
+    public Property findProperty () {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        String name = sc.next();
+
+        for(int i = 0; i < properties.length && properties[i] != null; i++){
+            if(properties[i].getName().equals(name)){
+                return properties[i];
+            }
+        }
+        return null;
+    }
+
+    public void changePriceOfProperty(double presentPrice, double newPrice) {
+        Property property = findPropertyPrice(presentPrice);
+        property.setPrice(newPrice);
+          }
+
+    public Property findPropertyPrice(double price) {
+        for(int i = 0; i < properties.length && properties[i] != null; i++){
+            if(properties[i].getPrice() == price) {
+                return properties[i];
+            }
+        }
+        return null;
+    }
+
+    public double calculateOfReduce(String name, double reduce){
+        double calculation = 0;
+        for(int i = 0; i < properties.length && properties[i] != null; i++){
+            calculation = (properties[i].getPrice() * reduce);
+               }
+        return calculation;
+    }
+
+    @Override
+    public String toString() {
+        return "OfficeOfProperty{" +
+                "iterator=" + iterator +
+                ", properties=" + Arrays.toString(properties) +
+                '}';
+    }
+}
