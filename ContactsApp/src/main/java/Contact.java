7ab860c042fcfe00a1d9f27965c9cd8694a1684c
Szymon Leyk <leyas@wp.pl>
2020-11-02 11:50:39 +0100

    #3 dodawanie kontaktów

--- ContactsApp/src/main/java/Contact.java
+++ ContactsApp/src/main/java/Contact.java
@@ -1,53 +1,14 @@
-import java.util.Scanner;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+
+@AllArgsConstructor
+@ToString
 public class Contact {
-
     private String name;
     private String surname;
     private String phoneNumber;
     private String email;
-
-    public Contact(String name, String surname, String phoneNumber, String email) {
-        this.name = name;
-        this.surname = surname;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    public Contact() {
-    }
-
-    public void addContact() throws MyException {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter name: ");
-        String name = sc.next();
-
-        System.out.println("Enter surname: ");
-        String surname = sc.next();
-
-        System.out.println("Enter phoneNumber:");
-        String phoneNumber = sc.next();
-
-        if(ValidationLab.validatePhoneNumber(phoneNumber) == false) {
-            throw new MyException("Wrong phone number!");
-        }
-
-        System.out.println("Enter email:");
-        String email = sc.next();
-
-        if(ValidationLab.validateEmail(phoneNumber) == true) {
-            throw new MyException("Wrong email!");
-        }
-
-        Contact contact = new Contact (name, surname, phoneNumber, email);
-
-    }
-
-    public String toString() {
-        return name + ", " + surname + ", " + phoneNumber + ", " + email;
-    }
 }
 
 /*Funkcjonalności:
--- ContactsApp/src/main/java/Main.java
+++ ContactsApp/src/main/java/Main.java
@@ -1,47 +1,27 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-
     private static final int STOP_PROGRAM = 0;
+    private static final int ADD = 1;
+    private static final int DISPLAY = 2;
 
     public static void main(String[] args) {
-
-        ArrayList<Contact> arrlist = new ArrayList<>();
-        arrlist.add(new Contact("ff", "fff", "fff", "gg"));
+        ContactBook contactBook = new ContactBook();
 
         Scanner sc = new Scanner(System.in);
-
-        for (Contact contact : arrlist) {
-            System.out.println(contact.toString());
-        }
-
         int option;
         do {
             System.out.println("--------------------------------");
+            System.out.println("0 - stop");
             System.out.println("1 - add contact");
             System.out.println("2 - display contacts");
-
             option = sc.nextInt();
 
             switch (option) {
-                case 1:
-                    try {
-                        for (Contact contact : arrlist) {
-                            contact.addContact();
-                        }
-
-                    } catch (MyException myexception) {
-                        myexception.printStackTrace();
-
-                        break;
-                    }
+                case ADD:
+                    contactBook.add();
+                    break;
             }
-        }
-            while (option != STOP_PROGRAM) ;
-
-
-
+        } while (option != STOP_PROGRAM);
     }
 }
